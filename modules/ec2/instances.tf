396832d240e38b21033e9147d82dcc273ec69dd9
Administrator <admin@example.com>
2019-06-28 16:26:17 +0200

    updating

--- modules/ec2/instances.tf
+++ modules/ec2/instances.tf
@@ -1,3 +1,5 @@
+#creation of the ec2
+
 resource "aws_instance" "web" {
   count         ="${var.ec2_count}"
   ami           = "${var.ami_id}"
@@ -7,3 +9,56 @@ resource "aws_instance" "web" {
     Name = "Hello"
   }
 }
+
+
+
+
+
+#creation of the ALB
+
+resource "aws_alb" "ALB" {
+
+  internal ="${var.internal}"
+#to get it from vars output vpc
+/* security_groups = "${var.security_groups}"  
+  subnets  = "${var.subnet}" */
+  enable_detection_protection = "${var.enable_detection_protection}"
+  tags = {
+    Name = "Load_balancer"
+}
+}
+
+
+
+
+#creation of the target group
+
+resource "aws_alb_target_group" "aws_targetgroup" {
+  vpc_id = "${var.vpc_id}"   
+  port = "${var.port}"
+  protocol = "${var.protocol}"
+  health_check {
+    path = "/healthcheck"
+    port = "${var.port}"
+    protocol = "${var.protocol}"
+    healthy_threshold = "${var.healthy_threshold}"
+    unhealthy_threshold = "${var.unhealthy_threshold}"
+    interval = "${var.interavl}"
+    timeout = "${var.timeout}"
+     }   
+  tags = {
+    Name = "alb_target_group "
+   
+}
+
+}
+
+
+
+#Assignement of the EC2 to the target group 
+
+resource "aws_alb_target_group_attachment"  "aws_targetgroup" {
+
+
+}
+
